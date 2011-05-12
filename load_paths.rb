1112f841a076daca0a19e5dd5911ceec55edf566
Davide D'Agostino <d.dagostino@lipsiasoft.com>
2011-05-12 22:23:31 +0200

    Removed deprecated things.

--- load_paths.rb
+++ load_paths.rb
@@ -1,7 +1,3 @@
-# We use this because we run these apps from our integration test where is not necessary
-# load bundler
-unless ENV['ALONE']
-  require 'rubygems' unless defined?(Gem)
-  require 'bundler'
-  Bundler.setup
-end
+require 'rubygems' unless defined?(Gem)
+require 'bundler'
+Bundler.setup
